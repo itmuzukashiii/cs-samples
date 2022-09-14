f7b984ca8ac88f7e057e3de4f061914326cbd221
default user <example@example.com>
2022-09-14 16:34:08 +0900

    add: csc.exe (C# 5.0) で WPF アプリをビルドしてみるサンプル

---
+++ csc/WpfWelcome/src/WpfWelcome.cs
@@ -0,0 +1,30 @@
+﻿// Windows 10 の .NET Framework 4 付属の csc.exe (C# 5.0) で WPF アプリを作る
+
+/*
+参考: https://ufcpp.net/study/csharp/st_basis.html
+*/
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfWelcome {
+    public class Program {
+
+        [STAThread]
+        static void Main() {
+            var button = new Button { Content = "Push Here" };
+            button.Click += (sender, e) => MessageBox.Show("Welcome!");
+      
+            var win = new Window {
+                Title = "Wpf Welcome"
+                ,Width = 300
+                ,Height = 200
+                ,Content = button
+            };
+        
+            var app = new Application();
+            app.Run(win);
+        }
+    }
+}
