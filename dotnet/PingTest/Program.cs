71da9d8443a11f7ff588e4e8243265faa7f67b75
default user <example@example.com>
2022-09-26 18:41:55 +0900

    new: PingTest
    System.Net.NetworkInformation.Ping サンプル

---
+++ dotnet/PingTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace PingTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(execPing(@"yahoo.com"));
+    }
+
+    private static bool execPing(string host)
+    {
+        var ping = new Ping();
+        var result = ping.Send(host, 2000);
+
+        if (result.Status == IPStatus.Success) {
+            Console.WriteLine($"{result.Status.ToString()}: Reply from {result.Address}: bytes={result.Buffer.Length} time={result.RoundtripTime}ms TTL={result.Options.Ttl}");
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
