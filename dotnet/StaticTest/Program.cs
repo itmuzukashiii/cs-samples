2d13b2916cf41efd44b17aafc50e41371b87eb10
default user <example@example.com>
2022-10-06 12:28:26 +0900

    new: static メンバのテスト

---
+++ dotnet/StaticTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StaticTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(StaticTest.Name);
+        StaticTest.Name = "Mary";
+        Console.WriteLine(StaticTest.Name);
+        // StaticTest.Age = 10; // 'StaticTest.Age' は読み取り専用であるため、割り当てることはできません
+    }
+}
+
+class StaticTest
+{
+    public static string Name = "John";
+    public static int Age {get;} = 29;
+}
+
+/*
+static メンバはインスタンス化なしに参照できる
+*/
