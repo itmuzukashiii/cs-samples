c9d9c50337d2c24ff606ef5e15b7476258904f92
default user <example@example.com>
2022-10-14 12:22:35 +0900

    new: TaskAwaitResult
    README追加

---
+++ dotnet/TaskAwaitResult/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskAwaitResult;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //--- result は long 型
+        var result = DoSomethingAsync().Result;
+        Console.WriteLine(result);
+    }
+  
+    private static async Task<long> DoSomethingAsync() {
+        var result = await Task.Run(() => {
+            long sum = 0;
+            for (int i = 1; i <= 10000000; i++) {
+                sum += i;
+            }
+            return sum;
+        });
+        //--- Task<long> 型非同期メソッドの return 式では long 型を返す
+        return result;
+    }
+}
